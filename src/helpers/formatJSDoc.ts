5081c8110f1bd1bebeed27c6ee0ee74aef67937e
Apisit Ritreungroj <apisit_lon@hotmail.com>
2024-01-02 00:11:37 +0700

    feat: more jsdoc on schema object

--- src/helpers/formatJSDoc.ts
+++ src/helpers/formatJSDoc.ts
@@ -1,24 +1,69 @@
+import { OpenAPIV3 } from "openapi-types";
+
+type PickedSchemaDescriptiveProperty =
+  | "description"
+  | "type"
+  | "format"
+  | "default"
+  | "deprecated"
+  | "example"
+  | "readOnly";
+
+type PickedSchemaConstraintProperty =
+  | "minimum"
+  | "exclusiveMinimum"
+  | "maximum"
+  | "exclusiveMaximum"
+  | "multipleOf"
+  | "maxLength"
+  | "minLength"
+  | "pattern"
+  | "minItems"
+  | "maxItems"
+  | "uniqueItems"
+  | "minProperties"
+  | "maxProperties";
+
 type Options = {
-  description: string;
-  type: string;
-  format: string;
-  default: string;
-  example: string;
-  deprecated: boolean;
   isMultiLineForced: boolean;
-}
+} & Required<Pick<OpenAPIV3.SchemaObject, PickedSchemaDescriptiveProperty | PickedSchemaConstraintProperty>>;
+
+type ConstraintOptions = Required<Pick<OpenAPIV3.SchemaObject, PickedSchemaConstraintProperty>>
 
 export default function formatJSDoc(options: Partial<Options> = {}): string {
   if (Object.values(options).every((x) => !x)) {
     return "";
   }
   else {
-    const { description, isMultiLineForced, deprecated, ...blockTags } = options;
+    const {
+      type,
+      format,
+      default: defaultValue,
+      example,
+      ...o
+    } = options;
+
+    const {
+      description,
+      isMultiLineForced,
+      deprecated,
+      readOnly,
+      ...contraints
+    } = o;
+
+    const formattedConstraint = formatConstraints(contraints);
 
     const lines = [
       description || "",
-      deprecated ? `@deprecated` : "",
-      ...Object.entries(blockTags).map(([ tag, value ]) => value ? `@${tag} ${value}` : "")
+      ...Object.entries({
+        type,
+        format,
+        default: defaultValue,
+        example
+      }).map(([ tag, value ]) => value ? `@${tag} ${value}` : ""),
+      formattedConstraint ? `@requires ${formattedConstraint}` : "",
+      readOnly ? `@readonly` : "",
+      deprecated ? `@deprecated` : ""
     ].filter(Boolean);
 
     if (lines.length === 0) {
@@ -32,5 +77,86 @@ export default function formatJSDoc(options: Partial<Options> = {}): string {
     } else {
       return `/** ${lines.join("")} */`;
     }
+  }
+}
+
+function formatConstraints(options: Partial<ConstraintOptions> = {}): string {
+  if (Object.values(options).every((x) => !x)) {
+    return "";
+  }
+  else {
+    const {
+      maximum,
+      exclusiveMinimum,
+      minimum,
+      exclusiveMaximum,
+      multipleOf,
+      maxLength,
+      minLength,
+      pattern,
+      minItems,
+      maxItems,
+      uniqueItems,
+      maxProperties,
+      minProperties
+    } = options;
+
+    const results: string[] = [];
+
+    const hasMin = typeof minimum === "number" || typeof exclusiveMinimum === "number";
+    const hasMax = typeof maximum === "number" || typeof exclusiveMaximum === "number";
+
+    if (hasMin || hasMax) {
+      const openBrace = typeof minimum === "number" ? "[" : "(";
+      const closingBrace = typeof maximum === "number" ? "]" : ")";
+      const min = hasMin ? String(minimum || exclusiveMinimum) : "-∞";
+      const max = hasMax ? String(maximum || exclusiveMaximum) : "∞";
+
+      const formattedInterval = `${openBrace}${min}, ${max}${closingBrace}`;
+      const finalFormat = `be in range of ${formattedInterval}`;
+
+      results.push(finalFormat);
+    }
+    if (typeof multipleOf === "number") {
+      results.push(`be a multiple of ${multipleOf}`);
+    }
+    if (typeof minLength === "number") {
+      const plural = minLength === 1 ? "character" : "characters";
+      results.push(`have at least ${minLength} ${plural}`);
+    }
+    if (typeof maxLength === "number") {
+      const plural = maxLength === 1 ? "character" : "characters";
+      results.push(`not exceed ${maxLength} ${plural}`);
+    }
+    if (typeof pattern === "string") {
+      results.push(`comply to /${pattern}/g pattern`);
+    }
+    if (typeof minItems === "number") {
+      const plural = minItems === 1 ? "item" : "items";
+      results.push(`have at least ${minItems} ${plural}`);
+    }
+    if (typeof maxItems === "number") {
+      const plural = maxItems === 1 ? "item" : "items";
+      results.push(`not exceed ${maxItems} ${plural}`);
+    }
+    if (uniqueItems === true) {
+      results.push(`need its items to be unique`);
+    }
+    if (typeof minProperties === "number") {
+      const plural = minProperties === 1 ? "property" : "properties";
+      results.push(`have at least ${minProperties} ${plural}`);
+    }
+    if (typeof maxProperties === "number") {
+      const plural = maxProperties === 1 ? "property" : "properties";
+      results.push(`not exceed ${maxProperties} ${plural}`);
+    }
+
+    const finalFormat = results.slice(1).reduce((acc, x, i) => {
+      return i === results.length - 2
+        ? `${acc} and ${x}`
+        : `${acc}, ${x}`;
+    }, results.at(0));
+    
+    return `The value must ${finalFormat}.`;
   }
 }
--- src/helpers/formatOASSchema.ts
+++ src/helpers/formatOASSchema.ts
@@ -3,11 +3,6 @@ import { OpenAPIV3 } from "openapi-types";
 import formatRefObject from "./formatRefObject";
 import formatJSDoc from "./formatJSDoc";
 
-type Result = {
-  jsDoc: string;
-  result: string;
-}
-
 /** Turns OpenAPI Schema Object into TypeScript types, displaying in string format. */
 export default function formatOASSchema(schemaObject: OpenAPIV3.SchemaObject): string {
   if (schemaObject.type === "object" && schemaObject.properties) {
@@ -26,12 +21,31 @@ export default function formatOASSchema(schemaObject: OpenAPIV3.SchemaObject): s
         ];
       } else {
         const jsDoc = formatJSDoc({
+          // annotation
           description: schema.description,
           deprecated: schema.deprecated,
           type: schema.type,
           format: schema.format,
           default: schema.default,
           example: schema.example,
+          // number
+          minimum: schema.minimum,
+          exclusiveMinimum: schema.exclusiveMinimum,
+          maximum: schema.maximum,
+          exclusiveMaximum: schema.exclusiveMaximum,
+          multipleOf: schema.multipleOf,
+          // string
+          maxLength: schema.maxLength,
+          minLength: schema.minLength,
+          pattern: schema.pattern,
+          // array
+          maxItems: schema.maxItems,
+          minItems: schema.minItems,
+          uniqueItems: schema.uniqueItems,
+          // object
+          minProperties: schema.minProperties,
+          maxProperties: schema.maxProperties,
+          // others
           isMultiLineForced: true
         });
 
@@ -53,14 +67,25 @@ export default function formatOASSchema(schemaObject: OpenAPIV3.SchemaObject): s
         ? formatRefObject(schemaObject.items)
         : formatOASSchema(schemaObject.items);
 
+      const hasMin = typeof schemaObject.minItems === "number";
+      const hasMax = typeof schemaObject.maxItems === "number";
+      const hasOnlyMax = !hasMin && hasMax;
+      const isEqualSize = hasMin && hasMax && schemaObject.minItems === schemaObject.maxItems;
+
+      // TODO:
+      if ((hasMin || hasMax || isEqualSize) && !hasOnlyMax) {
+
+      } else {
+
+      }
+
       return itemsType.includes("|")
         ? `(${itemsType})[]${orNullable}`
         : `${itemsType}[]${orNullable}`;
     } else if (schemaObject.type === "integer" || schemaObject.type === "number") {
       return `number${orNullable}`;
     } else if (schemaObject.enum) {
-      // TODO: add note for this to handle with non specific type: https://json-schema.org/understanding-json-schema/reference/enum
-      return `${schemaObject.enum.map((e) => schemaObject.type === "string" ? `"${e}"` : e).join(" | ")}${orNullable}`;
+      return `${schemaObject.enum.map((e) => typeof e === "string" ? `"${e}"` : e).join(" | ")}${orNullable}`;
     } else {
       return `${schemaObject.type}${orNullable}`;
     }
